be581e7e9dba28e8203051a073b65a2e8cd6aa4e
Alexey Simbarsky <asimbarsky@gmail.com>
2020-11-25 07:13:56 +0000

    Create rtf2pdf.js

---
+++ tools/rtf2pdf/rtf2pdf.js
@@ -0,0 +1,29 @@
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var docPath = WScript.Arguments(0);
+docPath = fso.GetAbsolutePathName(docPath);
+
+var pdfPath = docPath.replace(/\.rtf[^.]*$/, ".pdf");
+var objWord = null;
+
+try
+{
+    WScript.Echo("Saving '" + docPath + "' as '" + pdfPath + "'...");
+
+    objWord = new ActiveXObject("Word.Application");
+    objWord.Visible = false;
+
+    var objDoc = objWord.Documents.Open(docPath);
+
+    var wdFormatPdf = 17;
+    objDoc.SaveAs(pdfPath, wdFormatPdf);
+    objDoc.Close();
+
+    WScript.Echo("Done.");
+}
+finally
+{
+    if (objWord != null)
+    {
+        objWord.Quit();
+    }
+}
