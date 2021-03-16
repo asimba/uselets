6f33a4bc7bb823f011364e9497ece0512c86922d
Alexey Simbarsky <asimbarsky@gmail.com>
2021-03-16 13:50:50 +0000

    Add files via upload

---
+++ tools/ddefender/fix.cmd
@@ -0,0 +1,7 @@
+@echo off
+pushd "%CD%"
+CD /D "%~dp0"
+timeout 20
+powershell -ExecutionPolicy Bypass -command "&`%~dp0\fix.ps1"
+bcdedit /deletevalue {current} safeboot
+shutdown -f -r -t 0
---
+++ tools/ddefender/run.cmd
@@ -0,0 +1,21 @@
+@echo off
+:: BatchGotAdmin
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+    reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v "*ddf-fix" /t REG_SZ /d "%~dp0fix.cmd" /f
+    bcdedit /set {current} safeboot minimal
+    shutdown -f -r -t 0
