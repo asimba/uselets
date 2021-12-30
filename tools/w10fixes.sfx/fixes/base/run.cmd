ea367d577f6bd7710c71c57e3f11594f72d3576c
Alexey Simbarsky <asimbarsky@gmail.com>
2021-12-30 09:57:08 +0000

    Add files via upload

--- tools/w10fixes.sfx/fixes/base/run.cmd
+++ tools/w10fixes.sfx/fixes/base/run.cmd
@@ -10,6 +10,7 @@ schtasks /run /tn cmd_e >nul 2>&1
 schtasks /delete /tn cmd_c /f >nul 2>&1
 schtasks /delete /tn cmd_d /f >nul 2>&1
 schtasks /delete /tn cmd_e /f >nul 2>&1
+MsiExec.exe /q /X{73829F4D-CC8E-4FB4-A7F2-92430EC2210E} >nul 2>&1
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v "reboot" /t REG_SZ /d "shutdown -f -r -t 30" /f >nul 2>&1
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v "cleanup" /t REG_SZ /d "cmd.exe /c rmdir /q /s %SYSTEMDRIVE%\fixes" /f >nul 2>&1
 shutdown -f -r -t 5 >nul 2>&1
