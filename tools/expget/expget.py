7fe4d41e5ef5d2569f426d1b39cfd286b9ff7c56
Alexey Simbarsky <asimbarsky@gmail.com>
2022-04-30 18:29:48 +0000

    Add files via upload

--- tools/expget/expget.py
+++ tools/expget/expget.py
@@ -48,6 +48,10 @@ class CallHandler(QObject):
     def backrun_progress(self,counter):
         self.parent.progress_callback(counter)
 
+    @pyqtSlot(name="backrun_cancel")
+    def backrun_cancel(self):
+        self.parent.canceled_callback()
+
 class MainWindow(QWidget):
     def reload(self):
         if len(self.file_list): return
@@ -58,7 +62,8 @@ class MainWindow(QWidget):
             self.alllock=False
             self.label.setText('Статус:...')
             self.reload()
-            try: self.setEnabled(True)
+            try:
+                for b in self.buttons: b.setEnabled(True)
             except: pass
             return
         if event.value>1:
@@ -82,10 +87,22 @@ class MainWindow(QWidget):
                 self.label.setText('Статус: ошибка скачивания файла ("%s", код ошибки: %i)!'%(self.file_list[-1][-1],event.value))
                 self.alllock=False
                 self.reload()
-                try: self.setEnabled(True)
+                try:
+                    for b in self.buttons: b.setEnabled(True)
                 except: pass
 
+    def check_canceled(self,event):
+        if event: self.canceled_callback()
+
     def on_downloadRequested(self,event):
+        self.webEngineView.page().runJavaScript(\
+            """
+            function check_canceled(){
+                if(window.process_canceled) return 1;
+                else return 0;
+            };
+            check_canceled();
+            """,self.check_canceled)
         if event.state().value==0:
             event.stateChanged.connect(lambda event: self.on_downloadRequestState(event))
             event.accept()
@@ -108,8 +125,19 @@ class MainWindow(QWidget):
             self.label.setText('Статус:...')
             self.alllock=False
             self.reload()
-            try: self.setEnabled(True)
+            try:
+                for b in self.buttons: b.setEnabled(True)
             except: pass
+
+    def canceled_callback(self):
+        self.label.setText('Статус: операция отменена!')
+        self.alllock=False
+        self.file_list=[]
+        self.file_number=0
+        self.reload()
+        try:
+            for b in self.buttons: b.setEnabled(True)
+        except: pass
 
     def getfilelist_error_callback(self):
         self.label.setText('Статус: ошибка при получении списка файлов для скачивания!')
@@ -117,7 +145,8 @@ class MainWindow(QWidget):
         self.file_list=[]
         self.file_number=0
         self.reload()
-        try: self.setEnabled(True)
+        try:
+            for b in self.buttons: b.setEnabled(True)
         except: pass
 
     def getfilelist_success_callback(self,links):
@@ -146,12 +175,17 @@ class MainWindow(QWidget):
         if not('Files/prt0' in self.url): return
         self.file_list=[]
         self.file_number=0
-        try: self.setEnabled(False)
+        try:
+            for b in self.buttons: b.setEnabled(False)
         except: pass
         self.label.setText('Статус: получение списка файлов для скачивания...')
         self.webEngineView.page().runJavaScript(\
             self.qwebchannel_js+self.busy+"""
             function get_sig_links_details_parse(file_links,details_links,partitions,partition,section,data){
+                if(window.process_canceled){
+                    cancel();
+                    return;
+                };
                 var parser=new DOMParser();
                 var doc=parser.parseFromString(data,'text/html');
                 var req=doc.querySelectorAll('a[href*=\"GetFile\"]');
@@ -194,12 +228,17 @@ class MainWindow(QWidget):
                ('lk.spbexp.ru/SF/' in self.url and '/prt0/' in self.url)): return
         self.file_list=[]
         self.file_number=0
-        try: self.setEnabled(False)
+        try:
+            for b in self.buttons: b.setEnabled(False)
         except: pass
         self.label.setText('Статус: получение списка файлов для скачивания...')
         self.webEngineView.page().runJavaScript(\
             self.qwebchannel_js+self.busy+"""
             function get_sig_links_details_parse(file_links,details_links,partitions,partition,section,data){
+                if(window.process_canceled){
+                    cancel();
+                    return;
+                };
                 var parser=new DOMParser();
                 var doc=parser.parseFromString(data,'text/html');
                 var req=doc.querySelectorAll('a[href*=\"GetFile\"]');
@@ -213,6 +252,10 @@ class MainWindow(QWidget):
                 else get_file_links(file_links,details_links,partitions);
             };
             function get_pd_links_parse(path,file_links,details_links,partitions,data){
+                if(window.process_canceled){
+                    cancel();
+                    return;
+                };
                 var links=[];
                 for(const d of data.Data){
                     if(d['NumberOfFiles']){
@@ -224,6 +267,10 @@ class MainWindow(QWidget):
                 if(links.length) get_pd_links_details(links,file_links,details_links,partitions);
             };
             async function get_pd_links(path,file_links,details_links,partitions){
+                if(window.process_canceled){
+                    cancel();
+                    return;
+                };
                 var req=document.querySelector('a[href*="/Zeg/Zegmain1"]');
                 if(req){
                     var href=''.concat('https://lk.spbexp.ru/Grid/',path,'Read/own',req.pathname.split('\/').pop());
@@ -235,6 +282,10 @@ class MainWindow(QWidget):
                 };
             };
             function get_file_links(file_links,details_links,partitions){
+                if(window.process_canceled){
+                    cancel();
+                    return;
+                };
                 if(partitions.length) get_pd_links(partitions.pop(),file_links,details_links,partitions);
                 else success(file_links);
             };
@@ -253,8 +304,8 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('ExpGet')
-
         self.busy="""
+            window.process_canceled=false;
             function errlog(error){
                 window.qtwebchannel.backrun_error(error);
             };
@@ -264,7 +315,14 @@ class MainWindow(QWidget):
             function progress(file_links){
                 window.qtwebchannel.backrun_progress(file_links.length);
             };
+            async function cancel(){
+                window.qtwebchannel.backrun_cancel();
+            };
             async function get_sig_links_details(file_links,details_links,partitions){
+                if(window.process_canceled){
+                    cancel();
+                    return;
+                };
                 if(details_links.length){
                     var d=details_links.pop();
                     return await fetch(d[2])
@@ -275,6 +333,10 @@ class MainWindow(QWidget):
                 };
             };
             function get_pd_links_details_parse(links,file_links,details_links,partitions,partition,section,data){
+                if(window.process_canceled){
+                    cancel();
+                    return;
+                };
                 for(const d of data.Data){
                     file_links.push([partition,section,d['Nazvanie'],''.concat('https://lk.spbexp.ru/File/GetFile/',
                         d['IDRow']),d['sDateTo'],d['sMD5'],d['Number']]);
@@ -285,6 +347,10 @@ class MainWindow(QWidget):
                 else get_sig_links_details(file_links,details_links,partitions);
             };
             async function get_pd_links_details(links,file_links,details_links,partitions){
+                if(window.process_canceled){
+                    cancel();
+                    return;
+                };
                 if(links.length){
                     var d=links.pop();
                     return await fetch(d[2])
@@ -293,6 +359,9 @@ class MainWindow(QWidget):
                                     get_pd_links_details_parse(links,file_links,details_links,partitions,d[0],d[1],j);
                                 }).catch((error) => { errlog(error); });
                 };
+            };
+            function button_click(){
+                window.process_canceled=true;
             };
             class ProgressRing extends HTMLElement{
                 constructor(){
@@ -305,32 +374,77 @@ class MainWindow(QWidget):
                     this._root.innerHTML=`
                         <svg height="${radius*2}" width="${radius*2}">
                             <circle
+                                class="ring"
+                                stroke="#3c3b3a"
+                                stroke-width="${stroke*2}"
+                                stroke-opacity="0.5"
+                                fill="transparent"
+                                r="${normalizedRadius}"
+                                cx="${radius}"
+                                cy="${radius}"
+                                shape-rendering="geometricPrecision"
+                            />
+                            <circle
+                                class="ring"
                                 stroke="#c8c5c3"
                                 stroke-dasharray="${this._circumference} ${this._circumference}"
                                 style="stroke-dashoffset:${this._circumference}"
                                 stroke-width="${stroke}"
                                 fill="transparent"
-                                r="${normalizedRadius}"
+                                r="${normalizedRadius-2}"
                                 cx="${radius}"
                                 cy="${radius}"
+                                shape-rendering="geometricPrecision"
                             />
+                            <circle
+                                class="button"
+                                stroke="#191919"
+                                stroke-width="4"
+                                stroke-opacity="0.5"
+                                fill="#f44336"
+                                r="${normalizedRadius-stroke-2}"
+                                cx="${radius}"
+                                cy="${radius}"
+                                shape-rendering="geometricPrecision"
+                                onclick="button_click()"
+                            />
+                            <text class="txt" x="50%" y="52%" text-rendering="geometricPrecision">STOP</text>
                         </svg>
                         <style>
-                            circle {
+                            .ring {
+                                transition: stroke-dashoffset 0.35s;
                                 transform: rotate(-90deg);
                                 transform-origin: 50% 50%;
+                            }
+                            .button:hover {
+                                fill: #ce000f;
+                                opacity: 1;
+                            }
+                            .txt {
+                                font: bold 40px sans-serif;
+                                fill: #323232;
+                                stroke: #4c4c4c;
+                                stroke-width: 1px;
+                                text-anchor: middle;
+                                dominant-baseline: middle;
+                                pointer-events: none;
                             }
                         </style>
                     `;
                 }
                 setProgress(percent){
                     const offset=this._circumference-(percent/100*this._circumference);
-                    const circle=this._root.querySelector('circle');
+                    const circle=this._root.querySelectorAll('circle')[1];
                     circle.style.strokeDashoffset=offset;
                 }
-                static get observedAttributes(){ return ['progress']; }
+                setTransition(value){
+                    const circle=this._root.querySelectorAll('circle')[1];
+                    circle.style.transition='stroke-dashoffset '+value+'s';
+                }
+                static get observedAttributes(){ return ['progress','transition']; }
                 attributeChangedCallback(name,oldValue,newValue){
                     if(name==='progress') this.setProgress(newValue);
+                    if(name==='transition') this.setTransition(newValue);
                 }
             }
             function overlay() {
@@ -352,11 +466,11 @@ class MainWindow(QWidget):
                 document.getElementsByTagName('body')[0].appendChild(overlay);
                 window.customElements.define('progress-ring',ProgressRing);
                 const circle=document.createElement("div");
-                circle.innerHTML=`<progress-ring stroke="8" radius="60" progress="0"></progress-ring>`
+                circle.innerHTML=`<progress-ring stroke="8" radius="88" progress="0"></progress-ring>`
                 circle.style.top='50%';
                 circle.style.left='50%';
-                circle.style.marginTop='-60px'
-                circle.style.marginLeft='-60px'
+                circle.style.marginTop='-88px'
+                circle.style.marginLeft='-88px'
                 circle.style.zIndex='2000';
                 circle.style.position='absolute';
                 window.cprogress=0;
@@ -366,8 +480,16 @@ class MainWindow(QWidget):
             overlay();
             const interval=setInterval(()=>{
                 const el=document.querySelector('progress-ring');
-                window.cprogress=(window.cprogress+2)%200;
-                el.setAttribute('progress',window.cprogress);
+                if(window.cprogress==200){
+                    el.setAttribute('transition','0');
+                    window.cprogress=0;
+                    el.setAttribute('progress',window.cprogress);
+                }
+                else{
+                    if(window.cprogress==0) el.setAttribute('transition','0.35');
+                    window.cprogress+=2;
+                    el.setAttribute('progress',window.cprogress);
+                };
             },100);
         """
 
@@ -399,6 +521,7 @@ class MainWindow(QWidget):
         self.reload_button=QPushButton('Обновить страницу')
         self.get_data_button=QPushButton('Получить данные')
         self.get_all_data_button=QPushButton('Скачать данные проекта')
+        self.buttons=[self.reload_button,self.get_data_button,self.get_all_data_button]
         self.hbox.addWidget(self.reload_button,1)
         self.hbox.addWidget(self.get_data_button,1)
         self.hbox.addWidget(self.get_all_data_button,1)
