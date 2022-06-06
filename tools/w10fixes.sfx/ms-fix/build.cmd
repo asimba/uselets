db1ab8ba221cb2df907a665a286e60e9cec8005d
Alexey Simbarsky <asimbarsky@gmail.com>
2022-06-06 09:39:17 +0000

    Add files via upload

--- tools/w10fixes.sfx/ms-fix/build.cmd
+++ tools/w10fixes.sfx/ms-fix/build.cmd
@@ -4,5 +4,5 @@ gcc -Os -s -static -m32 -mwindows -march=i386 -mtune=i386 -Wall -fno-builtin -ft
 -fmerge-constants -fpack-struct=1 -falign-jumps=1 -falign-loops=1 -falign-functions=1 ^
 -ffast-math -fno-inline-small-functions -fno-stack-protector -fno-exceptions -fno-ident -nostartfiles ^
 -ffunction-sections -fdata-sections ^
--Wl,--entry=_start,--no-insert-timestamp,--gc-sections,--disable-dynamicbase,--disable-reloc,^
---section-alignment,16,--file-alignment,16 -o ms-fix.exe ms-fix.c
+-Wl,--entry=_start,--no-insert-timestamp,--gc-sections,--disable-dynamicbase,--disable-reloc ^
+-o ms-fix.exe ms-fix.c
