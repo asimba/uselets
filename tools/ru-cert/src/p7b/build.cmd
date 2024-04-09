2e0ee865b07fd4320084f5802b11475584832c85
asimba <asimbarsky@gmail.com>
2024-04-09 17:53:18 +0300

    Some code optimization

--- tools/ru-cert/src/p7b/build.cmd
+++ tools/ru-cert/src/p7b/build.cmd
@@ -1,4 +1,4 @@
 pack certs certs.qb
 tcc icert.c -o icert-sfx.exe -L./ -lshell32 -ladvapi32 -luser32 -m32 -Wl,-subsystem=windows
 upx --best --ultra-brute icert-sfx.exe
-copy /b icert-sfx.exe + certs.qb ru-certs-202308.exe
+copy /b icert-sfx.exe + certs.qb ru-certs-202403.exe
